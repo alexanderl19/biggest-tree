690b085c30cee14b52aa99afe7d2255a078d9128
Alexander Liu <a@alexanderliu.com>
2024-11-22 04:39:14 -0800

    fix: vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,12 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   site: "https://example.com",
   integrations: [mdx(), sitemap(), svelte()],
+  adapter: vercel(),
 });
