edb3bb608d71e207b811f8716e05155b11041b98
Jennifer <jennil38@uci.edu>
2024-11-21 22:06:48 -0800

    added tree files

---
+++ src/components/Tree/FiberContainer.jsx
@@ -0,0 +1,16 @@
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { Scene } from "./Scene";
+import { Ground } from "./Ground";
+
+export function FiberContainer() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+    <Canvas camera={{ position: [14.4666, 2.0365, 5.556165], fov: 40 }} shadows>
+      <Scene />
+      <Ground />
+      <OrbitControls minDistance={1} maxDistance={200} />
+    </Canvas>
+    </div>
+  );
+}
