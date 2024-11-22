be5e022b0c00b88f592f6e7f52b21888c73b7c43
Alexander Liu <a@alexanderliu.com>
2024-11-22 04:57:37 -0800

    feat: popup

---
+++ src/components/Day.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  const day = 1;
+  const text = "jenny is";
+</script>
+
+<div class="day-popup">
+  {day}
+  {text}
+</div>
+
+<style>
+  .day-popup {
+    z-index: 2;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    padding: 20px;
+    border-radius: 8px;
+    min-height: 512px;
+  }
+</style>
